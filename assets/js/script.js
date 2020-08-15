9434b86eeccfa9522781a1f1c8f939564a76fec9
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-08-15 15:28:41 -0600

    Create var to call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
 // Passwrod Generator 8-128 Characters
+
+//Variables
+var Length = "";
+var SpecialCharacter;
+var LowerCase;
+var UpperCase;
+var NumericalCharacters;
+
+// var characters
 var number = ["0","1","2","3","4","5","6","7","8","9"]
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var alphLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
+//Amount of Characters
+function generatePassword() {
+  var Length = (prompt("How Many Characters Would You like Your Password to Be?"));
+}
+
 
 
 // Get references to the #generate element

297078ca7e40a9c804ace14befe0c51ec0eaa497
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-08-15 15:18:11 -0600

    Add Character var

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
-// Assignment code here
+// Passwrod Generator 8-128 Characters
+var number = ["0","1","2","3","4","5","6","7","8","9"]
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var alphLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
 
 // Get references to the #generate element

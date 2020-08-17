115c8ca3ed9535eb4fe3822c82c496c961f70ca2
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-08-16 19:40:51 -0600

    add If statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,85 @@
-// Passwrod Generator 8-128 Characters
-
-//Variables
-var Length = "";
-var SpecialCharacter;
-var LowerCase;
-var UpperCase;
-var NumericalCharacters;
-
 // var characters
 var number = ["0","1","2","3","4","5","6","7","8","9"]
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmLowerCase;
+var confirmUpperCase;
+var confirmNumericalCharacters;
 
 //Amount of Characters
 function generatePassword() {
-  alert("Password Must Be Between The Parameters of 8-128")
-  var Length = (prompt("How Many Characters Would You like Your Password to Be?"));
+  alert("Press Generate for New Password")
 }
 
-while(Length <= 7 || Length >= 129 );{
-  var Length = (prompt("How Many Characters Would You Like your Password to Be?"));
+while(confirmLength <= 7 || confirmLength >= 129 ) {
+  alert("Password Must Be Between The Parameters of 8-128");
+  var confirmLength = (prompt("How Many Characters Would You Like your Password to Be?"));
 }
 
 // Tell User How Many Characters the Password will have
-alert('Your New Password WILL Contain $(Length) Characters');
+alert(`Your Password Will Contain ${confirmLength} characters`);
 
 // Password Character List
-var SpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-var NumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
-var LowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-var UpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
-  // Loop If Answers 
-  while(UpperCase === false && LowerCase === false && SpecialCharacter === false && NumericCharacter === false) {
-    alert("You must choose at least one parameter");
-    var SpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-    var NumericCharacter = confirm("Click OK to confirm if you would like to include numerbers");    
-    var LowerCase = confirm("Click OK to confirm if you would like to include lowercase letters");
-    var UpperCase = confirm("Click OK to confirm if you would like to include uppercase letters");   
-} 
+var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+var comfirmNumericalCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+var confirmLowerCase = confirm("Click OK to confirm if you would like to include confirmLowerCase characters");
+var confirmUpperCase = confirm("Click OK to confirm if you would like to include confirmUpperCase characters");
 
+var PassChar = []
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+ if (confirmSpecialCharacter && confirmNumericalCharacters && confirmLowerCase && confirmUpperCase) {
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+        PassChar = specialChar.concat(number, alphaUpper, alphaLower);
+    }
+    // Else if for 3 positive options
+    else if (confirmSpecialCharacter && confirmNumericalCharacters && confirmLowerCase) {
+        PassChar = specialChar.concat(number, alphaLower);
+    }
+    else if (confirmSpecialCharacter && confirmNumericalCharacters && confirmUpperCase) {
+        PassChar = specialChar.concat(number, alphaUpper);
+    }
+    else if (confirmSpecialCharacter && confirmUpperCase && confirmUpperCase) {
+        PassChar = specialChar.concat(alphaUpper, alphaLower);
+    }
+    else if (confirmNumericalCharacters && confirmUpperCase && confirmUpperCase) {
+        PassChar = number.concat(alphaUpper, alphaLower);
+    }
+    // Else if for 2 positive options 
+    else if (confirmSpecialCharacter && confirmNumericalCharacters) {
+        PassChar = specialChar.concat(number);
 
-  passwordText.value = password;
+    } else if (confirmSpecialCharacter && confirmUpperCase) {
+        PassChar = specialChar.concat(alphaUpper);
 
-}
+    } else if (confirmSpecialCharacter && confirmUpperCase) {
+        PassChar = specialChar.concat(alphaLower);
+    }
+    else if (confirmUpperCase && confirmNumericalCharacters) {
+        PassChar = alpha.concat(number);
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    } else if (confirmLowerCase && confirmUpperCase) {
+        PassChar = alpha.concat(alphaLower);
+
+    } else if (confirmNumericalCharacters && confirmUpperCase) {
+        PassChar = number.concat(alphaLower);
+    }
+    // Else if for 1 positive option
+    else if (confirmSpecialCharacter) {
+        PassChar = specialChar;
+    }
+    else if (confirmNumericalCharacters) {
+        PassChar = number;
+    }
+    else if (confirmUpperCase) {
+        PassChar = alphaUpper;
+    }
+    // Created space variable to fill confirmUpperCase conversion
+    else if (confirmLowerCase) {
+        PassChar = space.concat(alphaLower);
+    };
+
+  
